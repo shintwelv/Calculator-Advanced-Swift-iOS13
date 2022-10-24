ddf62898a93454dfe80c858aef648482c278fe48
elvinHeo <shinilheo0205@gmail.com>
2022-10-24 22:54:44 +0900

    complete calculator app

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by ShinIl Heo on 2022/10/24.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) -> Void {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n/100
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "÷":
+                return n1 / n2
+            case "×":
+                return n1 * n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        
+        return nil
+    }
+    
+}
